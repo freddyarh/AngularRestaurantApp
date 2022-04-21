fea4ab820d7d32db4b630ee455a544feae5b6841
Fredy Aristizabal <freddyarh13@gmail.com>
2022-04-21 09:27:13 -0500

    We added  a backdrop to the modal and a background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { JwtModule } from '@auth0/angular-jwt';
 import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 //NgRx
 import { StoreModule } from '@ngrx/store';
@@ -54,6 +55,7 @@ import { environment } from 'src/environments/environment';
     BrowserAnimationsModule,
     BrowserModule,
     MatButtonModule,
+    MatMenuModule,
     NoopAnimationsModule,
     AppRoutingModule,
     FormsModule,
